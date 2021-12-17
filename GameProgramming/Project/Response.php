01863c56a8aab39046fe7229249cbd7cc768099e
HanUseArch <wooyeophan@gmail.com>
2021-12-17 22:53:31 +0900

    Completed

--- GameProgramming/Project/Response.php
+++ GameProgramming/Project/Response.php
@@ -10,22 +10,23 @@
 //         #TYPE#$A=wa sans$B=ori;#TYPE#$A=I'm ori$B=jemmun;
 //         #LOGIN#$id=id$pw=pw;
 //         #SCORE#$id=id$score=score;
-
+ini_set("display_errors", 1);
 
 require "./lib/lib.han.php"; // loop statement
 require "../../lib/lib.common.php"; // debug only
 
 $post = $_POST;
 
-#region Null value check
 
+#region Null value check
+// $post["type"] = "USERITEM";
 if(empty($post["type"])) { // exception
     echo "#ERR#\$WHAT=No type found&;";
     exit();
 }
 
 $post["name"]   = empty($post["name"])   ? "no_id" : $post["name"];
-$post["itemId"] = empty($post["itemId"]) ? -1      : $post["itemId"];
+$post["itemId"] = empty($post["itemId"]) ? "no_id" : $post["itemId"];
 #endregion
 
 $type = $post["type"];
@@ -35,20 +36,25 @@ $queryResult;           // actual query result
 $row;                   // for fetch_row();
 $dbAccess;
 $i = 0;
+$canBuy = false;
 
 #region Queries
-$queryCheckUser      = "SELECT count(id) FORM `tb_gp` WHERE name='" . $post["name"] . "';";
-$queryGetUserData    = "SELECT u.name, u.gold, i.name FROM `tb_gp` AS u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i ON m.itemId=i.id WHERE u.name='" . $post["name"] . "';";
-$queryGetAllUserData = "SELECT u.name, u.gold         FROM `tb_gp` AS u;";
-$queryGetHan         = "SELECT u.name, u.gold, i.name FROM `tb_gp` AS u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i ON m.itemId=i.id WHERE u.name='Han';";
-$queryGetWeaponData  = "SELECT `name`, `price` FROM `tb_item` WHERE `type`=0;";
-$queryGetArmorData   = "SELECT `name`, `price` FROM `tb_item` WHERE `type`=1;";
-$queryGetUserItem = "SELECT"; // TODO : not finished
+$queryCheckUser       = "SELECT count(id) FORM `tb_gp` WHERE name='" . $post["name"] . "';";
+$queryGetUserData     = "SELECT u.name, u.gold, i.name FROM `tb_gp` AS u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i ON m.itemId=i.id WHERE u.name='" . $post["name"] . "';";
+$queryGetAllUserData  = "SELECT u.name, u.gold         FROM `tb_gp` AS u;";
+$queryGetHan          = "SELECT u.name, u.gold, i.name FROM `tb_gp` AS u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i ON m.itemId=i.id WHERE u.name='Han';";
+$queryGetWeaponData   = "SELECT `name`, `price`  FROM `tb_item` WHERE `type`=0;";
+$queryGetArmorData    = "SELECT `name`, `price`  FROM `tb_item` WHERE `type`=1;";
+$queryGetUserItem     = "SELECT i.type, i.name   FROM tb_gp as u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i on m.itemId=i.id WHERE u.name='". $post["name"] ."';";
+// $queryGetUserItem     = "SELECT i.type, i.name   FROM tb_gp as u LEFT JOIN tb_itemMappingTable AS m ON u.id=m.charactorId LEFT JOIN tb_item AS i on m.itemId=i.id WHERE u.name='오리';";
+$queryCanBuyItem      = "SELECT id FROM `tb_item` WHERE `name`='" . $post["itemId"] . "' AND `price` <= (SELECT `gold` FROM `tb_gp` WHERE `name`='" . $post["name"] . "');";
+// $queryCanBuyItem      = "SELECT COUNT(id) FROM `tb_item` WHERE `name`='신문지' AND `price` <= (SELECT `gold` FROM `tb_gp` WHERE `name`='오리');";
+$queryAddItemToUser   = "INSERT INTO `tb_itemMappingTable` (`charactorId`, `ItemId`) VALUES ((SELECT `id` FROM `tb_gp` WHERE `name`='" . $post["name"] . "'), (SELECT `id` FROM `tb_item` WHERE `name`='" . $post["itemId"]. "'));";
+$queryUpdateUserGold  = "UPDATE `tb_gp` SET `gold`=`gold`-(SELECT `price` FROM `tb_item` WHERE `name`='" . $post["itemId"] . "') WHERE `name`='". $post["name"] ."';";
 
-$queryAddItemToUser  = "INSERT INTO itemMappingTable(charactorId, ItemId) VALUES (" . $post["name"] . ", " . $post["itemId"] . ");";
 #endregion
 
-$dbAccess    = mysqli_connect("localhost", "han", "1234", "study_db") or die("Cannot connect to database server.\r\nQuitting");
+$dbAccess = mysqli_connect("localhost", "han", "1234", "study_db") or die("Cannot connect to database server.\r\nQuitting");
 
 #region Query
 switch($type)
@@ -73,13 +79,25 @@ switch($type)
         $queryResult = mysqli_query($dbAccess, $queryGetUserItem);
         break;
 
+    case "BUY":
+        $queryResult = mysqli_query($dbAccess, $queryCanBuyItem);
+
+        if(mysqli_fetch_row($queryResult) > 0) {
+            mysqli_query($dbAccess, $queryAddItemToUser);
+            mysqli_query($dbAccess, $queryUpdateUserGold);
+            echo "#POPUP#\$TEXT=Successfully bought item.\$CALLBACK=RETURNTOMAIN&;";
+        } else {
+            echo "#POPUP#\$TEXT=Not enought gold.&;";
+        }
+        exit();
+        break;
+
     default: // invalid $post["type"]
         echo "#ERR#\$WHAT=type is invalid&;";
         exit();
 
 }
 #endregion
-
 
 if ($queryResult) { // query result to array
     _while(function () {
@@ -96,7 +114,6 @@ if ($queryResult) { // query result to array
     mysqli_free_result($queryResult);
 }
 
-
 $i = 0;
 _repeat(count($fetchResult), function () { // array to string conversion (array to packet conversion)
     global $fetchResult;
@@ -108,7 +125,8 @@ _repeat(count($fetchResult), function () { // array to string conversion (array 
     {
         case "DATA":
         case "FULLDATA":
-            $response .= "#DATA#\$NAME=" . $fetchResult[$i][0] . "\$GOLD=" . $fetchResult[$i][1] . "\$WEAPON=" . $fetchResult[$i][2] . "&;";
+            // $response .= "#DATA#\$NAME=" . $fetchResult[$i][0] . "\$GOLD=" . $fetchResult[$i][1] . "\$WEAPON=" . $fetchResult[$i][2] . "&;";
+            $response .= "#DATA#\$NAME=" . $fetchResult[$i][0] . "\$GOLD=" . $fetchResult[$i][1] . "&;";
             break;
 
         case "GETARMOR":
@@ -116,7 +134,9 @@ _repeat(count($fetchResult), function () { // array to string conversion (array 
             $response .= "#WEAPONDATA#\$NAME=" . $fetchResult[$i][0] . "\$PRICE=" . $fetchResult[$i][1] . "&;";
             break;
 
-        
+        case "USERITEM":
+            $response .= "#USERITEM#\$TYPE=" . $fetchResult[$i][0] . "\$NAME=" . $fetchResult[$i][1] . "&;";
+            break;
 
         default:
             break;
